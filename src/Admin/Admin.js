4c0d9cdf393e2169d0146d6b6af37b964299fee7
nhanle2107 <75714814+nhanle2107@users.noreply.github.com>
2021-11-07 10:57:48 +0700

    First update Admin.js
    - Thay đổi lại Router
    - Thêm CustomerDetail

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,46 +1,28 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/NavbarAdmin';
+import NavbarAdmin from './components/NavbarAdmin';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/HomeAdmin';
 import Customers from './pages/Customer';
 import Order from './pages/order';
 import Support from './pages/support';
 import Warehouse from './pages/warehouse';
 import HomeAdmin from './pages/HomeAdmin';
-/*
 import CustomerDetail from './pages/Customer-detail';
-function App() { 
-  return (
-    <CustomerDetail/>
-  );
-}
-/*/
+
 function Admin() {
   return (
     <>
-
-          <Route path='/admin' exact>
-            <Navbar/>
-            <HomeAdmin/>
-            </Route>
-            <Route path='/admin/customer' >
-            <Navbar/>
-            <Customers/>
-            </Route>
-            <Route path='/admin/order' >
-            <Navbar/>
-            <Order/>
-            </Route>
-            <Route path='/admin/team' >
-            <Navbar/>
-            <Warehouse/>
-            </Route>
-            <Route path='/admin/support' exact>
-            <Navbar/>
-            <Support/>
-            </Route>
-
+      <Router>
+        <NavbarAdmin />
+        <Switch>
+          <Route path='/admin' exact component={HomeAdmin} />
+          <Route path='/admin/customer' exact component={Customers} />
+          <Route path='/admin/customer/detail' component={CustomerDetail}/>
+          <Route path='/admin/order' component={Order} />
+          <Route path='/admin/team' component={Warehouse} />
+          <Route path='/admin/support' component={Support} />
+        </Switch> 
+      </Router>
     </>
   );
 }
