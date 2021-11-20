2a8b1ee4f2e15e8095ea2a1c3a3bdb52a4531eec
nhanle2107 <lengocminhnhan@gmail.com>
2021-11-21 00:19:45 +0700

    Update CusData

--- src/Admin/pages/mockdata/CusData.js
+++ src/Admin/pages/mockdata/CusData.js
@@ -6,7 +6,7 @@ const CusDatabase = [
         id: "000001",
         name: "Nguyễn Văn Anh",
         phone: "0911111111",
-        total: 1000000,
+        total: 310000,
         sex: 0,             // Nam
         bdate: "01/01/2001",
         email: "nguyena@gmail.com"
@@ -16,7 +16,7 @@ const CusDatabase = [
         id: "000002",
         name: "Lê Văn Bình",
         phone: "0900000000",
-        total: 500000,
+        total: 90000,
         sex: 0,             // Nam
         bdate: "01/01/2001",
         email: "leb@gmail.com"
@@ -26,7 +26,7 @@ const CusDatabase = [
         id: "000003",
         name: "Trần Hoàng Công",
         phone: "0356789012",
-        total: 1500000,
+        total: 660000,
         sex: 0,             // Nam
         bdate: "01/01/2001",
         email: "tranc@gmail.com"
@@ -36,7 +36,7 @@ const CusDatabase = [
         id: "000004",
         name: "Đỗ Thị Duyên",
         phone: "0864213579",
-        total: 1000000,
+        total: 50000,
         sex: 1,             // Nữ
         bdate: "01/01/2001",
         email: "dod@gmail.com"
@@ -46,7 +46,7 @@ const CusDatabase = [
         id: "000005",
         name: "Hoàng Mỹ Dung",
         phone: "0511223344",
-        total: 100000,
+        total: 300000,
         sex: 1,             // Nữ
         bdate: "01/01/2001",
         email: "hoangmye@gmail.com"
@@ -56,7 +56,7 @@ const CusDatabase = [
         id: "000006",
         name: "Mai Văn Giang",
         phone: "0712458307",
-        total: 700000,
+        total: 410000,
         sex: 0,             // Nam
         bdate: "01/01/2001",
         email: "maig@gmail.com"
@@ -66,7 +66,7 @@ const CusDatabase = [
         id: "000007",
         name: "Đoàn Thanh Tâm",
         phone: "0907512568",
-        total: 700000,
+        total: 230000,
         sex: 1,             // Nữ
         bdate: "01/01/2001",
         email: "doantam@gmail.com"
@@ -76,7 +76,7 @@ const CusDatabase = [
         id: "000008",
         name: "Lâm Cao Đức",
         phone: "0915678423",
-        total: 700000,
+        total: 120000,
         sex: 0,             // Nam
         bdate: "01/01/2001",
         email: "lamduc@gmail.com"
@@ -86,7 +86,7 @@ const CusDatabase = [
         id: "000009",
         name: "Đào Minh Trí",
         phone: "0758946178",
-        total: 700000,
+        total: 150000,
         sex: 0,             // Nam
         bdate: "01/01/2001",
         email: "daotri@gmail.com"
@@ -96,7 +96,7 @@ const CusDatabase = [
         id: "000010",
         name: "Huỳnh Ngọc",
         phone: "0547823548",
-        total: 700000,
+        total: 100000,
         sex: 1,             // Nữ
         bdate: "01/01/2001",
         email: "huynhngoc@gmail.com"
