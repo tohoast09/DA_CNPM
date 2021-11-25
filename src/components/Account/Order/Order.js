41d9529cd3bf7c0d94e23dd415796a07bfb96874
dang-416 <59189549+dang-416@users.noreply.github.com>
2021-11-25 12:57:13 +0700

    get Order List

--- src/components/Account/Order/Order.js
+++ src/components/Account/Order/Order.js
@@ -1,39 +1,48 @@
 import OrderCard from "./OrderCard";
 // import "./Order.css";
-import order from './Order.module.css';
-import style from '../Account.module.css';
+import order from "./Order.module.css";
+import style from "../Account.module.css";
+import { useUserInfo } from "../../../context/getUserAPI";
 // import OrderDetail from "./OrderDetail/OrderDetail";
 
-const DUMMY_DATA = [
-    {
-        id: "123456789",
-        date: "01/01/2021",
-        description: "Some description.",
-        price: "200000đ", //tổng giá
-        status: "Hoàn thành",
-        className: "orderItem",
-        payment: "Paypal",
-        book: [{id:1, quantity:2, book_price:1234, img:'link'},{id:2, quantity:4, book_price:15000, img:'link'}]
-    },
-    {
-        id: "123456789",
-        date: "01/01/2021",
-        description: "Some description.",
-        price: "200000đ",
-        status: "Hoàn thành",
-        className: "orderItem",
-    },
-    {
-        id: "123456789",
-        date: "01/01/2021",
-        description: "Some description.",
-        price: "200000đ",
-        status: "Hoàn thành",
-        className: "orderItem",
-    },
-];
+// const DUMMY_DATA = [
+//     {
+//         id: "123456789",
+//         date: "01/01/2021",
+//         description: "Some description.",
+//         price: "200000đ", //tổng giá
+//         status: "Hoàn thành",
+//         className: "orderItem",
+//         payment: "Paypal",
+//         book: [{id:1, quantity:2, book_price:1234, img:'link'},{id:2, quantity:4, book_price:15000, img:'link'}]
+//     },
+//     {
+//         id: "123456789",
+//         date: "01/01/2021",
+//         description: "Some description.",
+//         price: "200000đ",
+//         status: "Hoàn thành",
+//         className: "orderItem",
+//     },
+//     {
+//         id: "123456789",
+//         date: "01/01/2021",
+//         description: "Some description.",
+//         price: "200000đ",
+//         status: "Hoàn thành",
+//         className: "orderItem",
+//     },
+// ];
 
 function Order() {
+    const { orders } = useUserInfo();
+    const getDescription = (order) => {
+        const firstbook = order.data.books[0].id;
+        const numberleft = order.data.books.length - 1;
+        if (numberleft)
+            return firstbook + " và " + numberleft + " sản phẩm khác";
+        else return firstbook;
+    };
     return (
         <div className={order.UserOrder}>
             <h1>Đơn hàng của tôi</h1>
@@ -41,21 +50,33 @@ function Order() {
                 <div className={style.orderInsideContent}>
                     <div className={order.OrderList}>
                         <div className={`${order.orderCard} ${order.bar}`}>
-                            <div className={order.orderId}><span>Mã đơn</span></div>
-                            <div className={order.orderDate}><span>Ngày đặt</span></div>
-                            <div className={`${order.orderDescription} ${order.bar}`}><span>Mô tả</span></div>
-                            <div className={order.orderPrice}><span>Giá</span></div>
-                            <div className={order.orderStatus}><span>Trạng thái</span></div>
+                            <div className={order.orderId}>
+                                <span>Mã đơn</span>
+                            </div>
+                            <div className={order.orderDate}>
+                                <span>Ngày đặt</span>
+                            </div>
+                            <div
+                                className={`${order.orderDescription} ${order.bar}`}
+                            >
+                                <span>Mô tả</span>
+                            </div>
+                            <div className={order.orderPrice}>
+                                <span>Giá</span>
+                            </div>
+                            <div className={order.orderStatus}>
+                                <span>Trạng thái</span>
+                            </div>
                         </div>
 
-                        {DUMMY_DATA.map((order) => {
+                        {orders.map((order) => {
                             return (
                                 <OrderCard
                                     id={order.id}
-                                    date={order.date}
-                                    description={order.description}
-                                    price={order.price}
-                                    status={order.status}
+                                    date={order.data.date}
+                                    description={getDescription(order)}
+                                    cost={order.data.cost}
+                                    status={order.data.status}
                                 />
                             );
                         })}
--- src/components/Account/Order/OrderCard.js
+++ src/components/Account/Order/OrderCard.js
@@ -8,7 +8,7 @@ function OrderCard(props) {
             <div className={classes.orderId}><span>{props.id}</span></div>
             <div className={classes.orderDate}><span>{props.date}</span></div>
             <div className={classes.orderDescription}><span>{props.description}</span><Link to='/account/orderdetail'>Chi tiết</Link></div>
-            <div className={classes.orderPrice}><span>{props.price}</span></div>
+            <div className={classes.orderPrice}><span>{props.cost}</span></div>
             <div className={classes.orderStatus}><span>{props.status}</span></div>
         </div>
         
