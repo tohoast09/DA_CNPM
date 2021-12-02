2c347361e96314ddf1073538a4217f67046c8d96
dang-416 <dang.nguyen.0416@hcmut.edu.vn>
2021-12-03 01:33:05 +0700

    ...

--- src/components/Account/AccountNav/AccountNavigation.js
+++ src/components/Account/AccountNav/AccountNavigation.js
@@ -2,6 +2,11 @@
 import { NavLink, Link } from "react-router-dom";
 import classes from "../Account.module.css";
 import { useUserInfo } from "../../../assets/firebase-data/getUserAPI";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ReceiptLongOutlinedIcon from "@mui/icons-material/ReceiptLongOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 
 function AccountNavigation() {
     const { userInfo } = useUserInfo();
@@ -12,10 +17,11 @@ function AccountNavigation() {
                     <div
                         className={classes.img}
                         style={{
-                            background: userInfo.img?`url(${userInfo.img}) center / cover no-repeat`:`url("https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0=") center / cover no-repeat`
+                            background: userInfo.img
+                                ? `url(${userInfo.img}) center / cover no-repeat`
+                                : `url("https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0=") center / cover no-repeat`,
                         }}
                     ></div>
-                    {/* <NavLink to='/account/changeavatar' className={navData=>navData.isActive?classes.avtBtnActive:classes.avtBtn}>Sửa ảnh đại diện</NavLink> */}
                 </div>
                 <NavLink
                     end
@@ -27,6 +33,7 @@ function AccountNavigation() {
                     to="/account"
                 >
                     {" "}
+                    <AccountCircleOutlinedIcon className={classes.icon} />
                     <span>Thông tin cá nhân</span>{" "}
                 </NavLink>
                 <NavLink
@@ -38,6 +45,7 @@ function AccountNavigation() {
                     to="/account/noti/"
                 >
                     {" "}
+                    <NotificationsNoneOutlinedIcon className={classes.icon} />
                     <span>Thông báo</span>{" "}
                 </NavLink>
                 <NavLink
@@ -49,6 +57,7 @@ function AccountNavigation() {
                     to="/account/orders/"
                 >
                     {" "}
+                    <ReceiptLongOutlinedIcon className={classes.icon} />
                     <span>Danh sách đơn hàng</span>{" "}
                 </NavLink>
                 <NavLink
@@ -60,6 +69,7 @@ function AccountNavigation() {
                     to="/account/address/"
                 >
                     {" "}
+                    <LocationOnOutlinedIcon className={classes.icon} />
                     <span>Danh sách địa chỉ</span>{" "}
                 </NavLink>
                 <NavLink
@@ -71,6 +81,9 @@ function AccountNavigation() {
                     to="/account/wallet/"
                 >
                     {" "}
+                    <AccountBalanceWalletOutlinedIcon
+                        className={classes.icon}
+                    />
                     <span>Ví</span>{" "}
                 </NavLink>
             </div>
